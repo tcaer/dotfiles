0f985bd469d1ff3ed7fe087e6adb5248099457d8
tcaer <tino.caer@me.com>
2023-01-07 23:29:45 -0500

    Initial Comimt

---
+++ nvim/init.lua
@@ -0,0 +1,90 @@
+-- Editor settings
+vim.wo.number = true
+vim.wrap = false
+
+-- Syntax settings
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+
+vim.o.smartindent = true
+vim.o.updatetime = 50
+vim.o.colorcolumn = "80"
+vim.o.signcolumn = "no"
+
+-- Packages
+vim.cmd [[packadd packer.nvim]]
+require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use 'morhetz/gruvbox'
+  use 'nvim-treesitter/nvim-treesitter'
+  use 'rust-lang/rust.vim'
+  use 'junegunn/fzf'
+  use 'junegunn/fzf.vim'
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    requires = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},
+      {'williamboman/mason.nvim'},
+      {'williamboman/mason-lspconfig.nvim'},
+
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-buffer'},
+      {'hrsh7th/cmp-path'},
+      {'saadparwaiz1/cmp_luasnip'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'hrsh7th/cmp-nvim-lua'},
+
+      -- Snippets
+      {'L3MON4D3/LuaSnip'},
+      -- Snippet Collection (Optional)
+      {'rafamadriz/friendly-snippets'},
+    }
+  }
+end)
+
+-- Theme settings
+vim.o.background = "dark"
+vim.cmd [[colorscheme gruvbox]]
+
+-- Mappings
+local opts = { noremap=true, silent=true }
+vim.g.mapleader = ' '
+---- fzf
+vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Files<cr>', opts)
+vim.api.nvim_set_keymap('n', '<C-t>', '<cmd>GFiles<cr>', opts)
+
+-- LSP
+local lsp = require('lsp-zero')
+lsp.preset('recommended')
+
+lsp.ensure_installed({
+  'sumneko_lua',
+  'rust_analyzer'
+})
+
+lsp.on_attach(function(client, bufnr)
+  local opts = {buffer = bufnr, remap = false}
+
+  if client.name == "eslint" then
+    vim.cmd.LspStop('eslint')
+    return
+  end
+
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+  vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
+  vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
+  vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
+  vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
+  vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
+  vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
+  vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
+  vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+end)
+
+lsp.setup()
+
