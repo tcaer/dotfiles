16450110208624379cd86e00135b7624eb0cd2b6
tcaer <tino.caer@me.com>
2023-08-29 17:43:09 -0700

    Update init.lua file

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,91 +1,74 @@
--- Editor settings
-vim.wo.number = true
-vim.opt.wrap = false
-
--- Syntax settings
+-- Basic vim stuff
+vim.g.mapleader = " "
+vim.opt.number = true
+vim.opt.mouse = 'a'
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.breakindent = true
 vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
 
-vim.opt.smartindent = true
-vim.opt.updatetime = 50
-vim.opt.colorcolumn = "80"
-vim.opt.signcolumn = "no"
+-- Lazy initialization
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
--- Packages
-vim.cmd [[packadd packer.nvim]]
-require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use 'morhetz/gruvbox'
-  use 'nvim-treesitter/nvim-treesitter'
-  use 'rust-lang/rust.vim'
-  use 'DingDean/wgsl.vim'
-  use 'junegunn/fzf'
-  use 'junegunn/fzf.vim'
-  use {
-    'VonHeikemen/lsp-zero.nvim',
-    requires = {
-      -- LSP Support
-      {'neovim/nvim-lspconfig'},
-      {'williamboman/mason.nvim'},
-      {'williamboman/mason-lspconfig.nvim'},
-
-      -- Autocompletion
-      {'hrsh7th/nvim-cmp'},
-      {'hrsh7th/cmp-buffer'},
-      {'hrsh7th/cmp-path'},
-      {'saadparwaiz1/cmp_luasnip'},
-      {'hrsh7th/cmp-nvim-lsp'},
-      {'hrsh7th/cmp-nvim-lua'},
-
-      -- Snippets
-      {'L3MON4D3/LuaSnip'},
-      -- Snippet Collection (Optional)
-      {'rafamadriz/friendly-snippets'},
-    }
-  }
-end)
-
--- Theme settings
-vim.o.background = "dark"
-vim.cmd [[colorscheme gruvbox]]
-
--- Mappings
-local opts = { noremap=true, silent=true }
-vim.g.mapleader = ' '
----- fzf
-vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Files<cr>', opts)
-vim.api.nvim_set_keymap('n', '<C-t>', '<cmd>GFiles<cr>', opts)
-
--- LSP
-local lsp = require('lsp-zero')
-lsp.preset('recommended')
-
-lsp.ensure_installed({
-  'sumneko_lua',
-  'rust_analyzer'
+-- Lazy plugins
+require("lazy").setup({
+	{
+		'nvim-telescope/telescope.nvim',
+		dependencies = { 'nvim-lua/plenary.nvim' }
+	},
+	{
+		'pmizio/typescript-tools.nvim',
+		dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+	},
+	'lewis6991/gitsigns.nvim',
+	'tpope/vim-fugitive',
+	{ 'ellisonleao/gruvbox.nvim', priority = 1000 },
 })
 
-lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+-- Gitsigns config
+require('gitsigns').setup()
 
-  if client.name == "eslint" then
-    vim.cmd.LspStop('eslint')
-    return
-  end
+-- Gruvbox config
+vim.o.background = 'dark'
 
-  vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-  vim.keymap.set("n", "df", vim.lsp.buf.hover, opts)
-  vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
-  vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
-  vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
-  vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
-  vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
-  vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
-  vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
-  vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
-end)
+require('gruvbox').setup({
+	italic = {
+		strings = false,
+		comments = false,
+		operators = false,
+		folds = false,
+	},
+	contrast = 'soft',
+})
+vim.cmd('colorscheme gruvbox')
 
-lsp.setup()
+-- LSP config
+vim.keymap.set('n', 'do', vim.diagnostic.open_float)
+vim.keymap.set('n', 'd]', vim.diagnostic.goto_next)
+vim.keymap.set('n', 'd[', vim.diagnostic.goto_prev)
 
+vim.keymap.set('n', 'gD', vim.lsp.buf.declaration)
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
+vim.keymap.set('n', 'gi', vim.lsp.buf.implementation)
+
+-- Telescope config
+local telescope = require('telescope.builtin')
+vim.keymap.set('n', '<C-p>', telescope.find_files)
+vim.keymap.set('n', '<C-l><C-p>', telescope.live_grep)
+vim.keymap.set('n', '<C-l>p', telescope.live_grep)
+
+-- Typescript
+require('typescript-tools').setup {
+}
