99df3062a76c36b7e800d735e610ebe14e722911
tcaer <tino.caer@me.com>
2024-03-18 21:05:39 -0700

    nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -74,9 +74,6 @@ require('gruvbox').setup({
     operators = false,
     folds = false,
   },
-  palette_overrides = {
-    light0 = '#f9f5d7'
-  },
 })
 
 local auto_dark_mode = require('auto-dark-mode')
@@ -84,7 +81,7 @@ local auto_dark_mode = require('auto-dark-mode')
 auto_dark_mode.setup({
   update_interval = 1000,
   set_dark_mode = function()
-    vim.api.nvim_set_option('background', 'dark')
+    vim.api.nvim_set_option('background', 'light')
     vim.cmd([[colorscheme gruvbox]])
   end,
   set_light_mode = function()
